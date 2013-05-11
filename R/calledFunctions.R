ed48d16652b41e69dfcd5fd8834c853227ca4b90
jonathancornelissen <jonathan.cornelissen@econ.kuleuven.be>
2013-05-11 22:35:30 +0200

    calledFunctions not finished yet, checkList finished

---
+++ R/calledFunctions.R
@@ -0,0 +1,34 @@
+calledFunctions <- function( user.code, recursive = FALSE, checked.functions = NULL){
+  
+  # break code up into sections preceding left brackets:
+  left.brackets <- c(unlist( strsplit(user.code, split="[[:space:]]*\\(")) )
+  
+  called.functions <- unique(c(unlist(sapply(left.brackets, 
+                                             function (x) {
+                                               
+                                               # Split up according to anything that can't be in a function name.
+                                               # split = not alphanumeric, not '_', and not '.'
+                                               words <- c(unlist(strsplit(x, split="[^[:alnum:]_.]")))
+                                               
+                                               last.word <- tail(words, 1)
+                                               last.word.is.function <- tryCatch(is.function(get(last.word)),
+                                                                                 error=function(e) return(FALSE))
+                                               return(last.word[last.word.is.function])
+                                             }))))
+  
+  if (recursive){
+    
+    # checked.functions: We need to keep track of which functions 
+    # we've checked to avoid infinite loops.
+    functs.to.check <- called.functions[!(called.functions %in%
+                                            checked.functions)]
+    
+    called.functions <- unique(c(called.functions,
+                                 do.call(c, lapply(functs.to.check, function(x) {
+                                   listFunctions(x, recursive = T,
+                                                 checked.functions = c(checked.functions,          
+                                                                       called.functions))
+                                 }))))
+  }
+  return(called.functions)
+}
--- R/checkList.R
+++ R/checkList.R
@@ -1,7 +1,10 @@
 checkList = function(name,value){
+  
+  # Check existence of variables in workspace:
   check <- checkExistence(name)
   if(!is.null(check)){ return(check) }
   
+  # Get the values:
   the.name  <- eval(parse(text=name));
   the.value <- eval(parse(text=value));
   
@@ -11,11 +14,19 @@ checkList = function(name,value){
     return(list(FALSE,to.print))
   }
   
-  n <- length(the.value); equal <- NULL;
+  # Check correct naming: 
+  if(!is.null(names(the.value))){ 
+    condition <- is.null(names(the.name)) | any(names(the.value) != names(the.name))
+    if(condition){
+      to.print <- paste("The list",name,"does not seem to have the correct names!")
+      return(list(FALSE,to.print))
+    }
+  }
   
+  n <- length(the.value); equal <- NULL;
   for(i in 1:n){
     equal[i] <- checkEqual(the.name[[i]],the.value[[i]])
-    if( !equal[i] ){  to.print <- capture.output( cat("Did you set the value of",names[[i]],"equal to:", values[[i]],"? ") ) }
+    if( !equal[i] ){  to.print <- capture.output( cat("Please check the value of list item:",i,"of the list:",name,"!") ) }
   }
   
   if(any(!equal)){ return( list(FALSE,to.print) ) } else { return( TRUE ) }  
