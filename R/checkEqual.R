270455208824d333f8c4a0c405351949961590e5
jonathancornelissen <jonathan.cornelissen@econ.kuleuven.be>
2013-05-13 15:48:22 +0200

    first alpha version to test on platform

--- R/checkEqual.R
+++ R/checkEqual.R
@@ -1,12 +1,17 @@
 checkEqual = function(the.name,the.value){
   NAS       <-  any(is.na(the.value))   |  any(is.na(the.name))
   NULLS     <-  any(is.null(the.value)) |  any(is.null(the.name))
-  
+  factors   <-  is.factor(the.value) & is.factor(the.name)
+    
   if(NAS){ #TODO: make NA handling better
     equal <- any(is.na(the.value)) &  any(is.na(the.name))  
   } else if(NULLS){  #TODO make NULL handling better   
     equal <- any(is.null(the.value)) &  any(is.null(the.name))
-  }else{ equal <- all(the.name == the.value) }
-  
+  }else{
+    if(factors){ # Check factor levels are the same, otherwise we get error on comparison
+      if( ! all(levels(the.value)==levels(the.name))){ return(FALSE) } 
+    }
+    equal <- all(the.name == the.value) 
+  }  
   return(equal)
 }
