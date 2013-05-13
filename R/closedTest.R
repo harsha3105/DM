458c4f78bbcdd9768d0a8b588f57a179b4dccdbc
jonathancornelissen <jonathan.cornelissen@econ.kuleuven.be>
2013-05-13 18:43:12 +0200

    closedTest now works with lists too

--- R/closedTest.R
+++ R/closedTest.R
@@ -1,31 +1,33 @@
 closedTest <- function( names=NULL, values=NULL, messages=NULL, check.existence=NULL){
-  if(is.null(check.existence)){ check.existence = rep(TRUE,length(names)) }
-  
-  # Do the variables to check exist in user workspace?
-  check <- checkExistence(names[check.existence])
-  
+
+  if(!is.null(names) & !is.null(values)){   # Check wheter arguments were supplied
+    
+  if(is.null(check.existence)){ check.existence = rep(TRUE,length(names)) }  
+  # Do the variables exist in user workspace?
+  check <- checkExistence(names[check.existence])  
   if(!is.null(check)){ return(check) }
   
-  if(!is.null(names) & !is.null(values)){   # Check wheter arguments were supplied
-    if( length(names) != length(values) ){ 
-      stop("There seems to be an error in the submission correctness test. Please contact the course creator.")
-    } 
+  if( length(names) != length(values) ){     stop("There seems to be an error in the submission correctness test. Please contact the course creator.") } 
     
     to.print   <- equal <- NULL; 
     the.names  <- lapply( names,  function(zzzzA){ eval(parse(text=zzzzA))} )
     the.values <- lapply( values, function(zzzzA){ eval(parse(text=zzzzA))} )
     n          <- length(names)
 
-    for(i in 1:n){ 
+    for(i in 1:n){ # Loop over arguments to check whether the student provided correct solution
       the.value <- the.values[[i]]
       the.name  <- the.names[[i]]
-      equal[i]  <- checkEqual(the.name,the.value)
+      we.have.lists <- is.list(the.value) & is.list(the.name)
       
-      # Print message:
-      if( !equal[i] ){  
-        to.print <- capture.output( cat("Did you set the value of",names[[i]],"equal to:", values[[i]],"? ") ) 
+      if(we.have.lists){ 
+        LC <- checkList(names[[i]],values[[i]])
+        equal[i] <- LC[[1]]
+        if(!equal[i]){ to.print <- LC[[2]] }
       }
-      
+      if(!we.have.lists){
+        equal[i]  <- checkEqual(the.name,the.value)
+        if( !equal[i] ){  to.print <- capture.output( cat("Did you set the value of",names[[i]],"equal to:", values[[i]],"? ") ) }
+      }
       }
     if(any(!equal)){ return( list(FALSE,to.print) ) } 
   }
